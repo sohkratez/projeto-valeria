74ba47d060ef60bc04353655239d5c8c6cbe9b3e
Pablo Luciano <125380107+sohkratez@users.noreply.github.com>
2024-12-11 10:03:15 -0300

    projeto atualizado

--- menu.js
+++ menu.js
@@ -1,15 +1,23 @@
-let btnMenu = document.getElementById('btn-menu')
-let menu = document.getElementById('menu-mobile')
-let overlay = document.getElementById('overlay-menu')
+const btnMenu = document.getElementById('btn-menu');
+const menu = document.getElementById('menu-mobile');
+const overlay = document.getElementById('overlay-menu');
+const btnFechar = menu.querySelector('.btn-fechar');
+const menuLinks = menu.querySelectorAll('nav ul li a')
 
-btnMenu.addEventListener('click', ()=>{
-    menu.classList.add('abrir-menu')
+function abrirMenu() {
+    menu.classList.add('abrir-menu');
+    overlay.style.display = 'block';
+}
+
+function fecharMenu() {
+    menu.classList.remove('abrir-menu');
+    overlay.style.display = 'none';
+}
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', fecharMenu);
 })
 
-menu.addEventListener('click', ()=>{
-    menu.classList.remove('abrir-menu')
-})
-
-overlay.addEventListener('click', ()=>{
-    menu.classList.remove('abrir-menu')
-})
+btnMenu.addEventListener('click', abrirMenu);
+btnFechar.addEventListener('click', fecharMenu);
+overlay.addEventListener('click', fecharMenu);
